f6c89ed4cf73456b2aa8a19238402fa00bf497fe
abdusharip <abdusharipa9@gmail.com>
2024-04-09 21:03:18 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const usz = document.querySelector('#usz'),
+      usd = document.querySelector('#usd')
+
+usz.addEventListener('input', (e) => {
+  // console.log(e);
+  const request = new XMLHttpRequest()
+
+  request.open('GET', 'current.json')
+  request.setRequestHeader('Content-Type', 'application/js; charset=utf-8')
+  request.send()
+
+  request.addEventListener('load', () => { // readystatechange ~ load => 4
+    if(request.status === 200){ // request.readyState === 4 && 
+      // console.log(request.response);
+      const date = JSON.parse(request.response)
+      usd.value = (+usz.value / date.current.usd).toFixed(3)
+    } else {
+      usd.value = 'Something went wrong'
+    }
+  })
+}) 
+
+ // status 200 - ok, 404 - not found, 500 - server, 400 - client error
+  // statusText 
+  // response   => 100~599
+  // readyStage => 1, 2, 3, 4
